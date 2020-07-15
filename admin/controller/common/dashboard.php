27777e842c124776a84a6ccca5bc0a123e1607b6
BecauseSo <liuxiaojie_sz@163.com>
2020-07-15 12:16:34 +0800

    取消仪表盘提示

--- admin/controller/common/dashboard.php
+++ admin/controller/common/dashboard.php
@@ -76,12 +76,8 @@ class ControllerCommonDashboard extends Controller {
 			}
 		}
 
-		if (DIR_STORAGE == DIR_SYSTEM . 'storage/') {
-			$data['security'] = $this->load->controller('common/security');
-		} else {
-			$data['security'] = '';
-		}
-		
+		$data['security'] = '';
+
 		$data['header'] = $this->load->controller('common/header');
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
